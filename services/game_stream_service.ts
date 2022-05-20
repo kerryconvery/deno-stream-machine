1cabe5cb55cd7722e569a9d090ef3c0d200550f0
Kerry Convery <kerry.convery@iag.com.au>
2022-05-20 21:04:23 +1000

    combines the results from each provider into a single aggregated list

--- services/game_stream_service.ts
+++ services/game_stream_service.ts
@@ -1,10 +1,26 @@
 
-export class Stream {
+export type Stream = {
 
 }
 
+export type PagedStreams = {
+  streams: Stream[],
+  nextPageToken?: string
+}
+
+export type GameStreamProviderOptions = {
+  pageSize: number,
+  pageSkip: number
+}
+
 export interface GameStreamProvider {
-  getStreams(): Stream[]
+  get id(): string
+  getStreams(options: GameStreamProviderOptions): PagedStreams,
+}
+
+export type GameStreamServiceOptions = {
+  pageSize: number,
+  pageSkip: number
 }
 
 export class GameStreamService {
@@ -16,9 +32,53 @@ export class GameStreamService {
     return this
   }
 
-  getStreams(): Stream[] {
-    return this.streamProviders.reduce<Stream[]>((allStreams, provider) => {
-      return allStreams.concat(provider.getStreams())
+  getStreams(options: GameStreamServiceOptions): PagedStreams {
+    const streamAggregator = new StreamAggregator()
+
+    this.streamProviders.forEach((provider) => {
+      streamAggregator.addStreams(
+        provider.id,
+        provider.getStreams({ pageSize: options.pageSize, pageSkip: options.pageSkip })
+      )
+    })
+
+    return streamAggregator.aggregateStreams()
+  }
+}
+
+class StreamAggregator {
+  private providerStreams: Record<string, PagedStreams> = {}
+  
+  public addStreams(providerId: string, streams: PagedStreams): StreamAggregator {
+    this.providerStreams[providerId] = streams
+
+    return this
+  }
+
+  public aggregateStreams(): PagedStreams {
+    return {
+      streams: this.getConcatenatedStreams(),
+      nextPageToken: this.getConcatenatedNextPageToken()
+    }
+  }
+
+  private getConcatenatedStreams(): Stream[] {
+    return Object
+      .values(this.providerStreams)
+      .reduce<Stream[]>((allStreams: Stream[], pagedStreams: PagedStreams) => {
+        return allStreams.concat(pagedStreams.streams)
     }, [])
+  }
+
+  private getConcatenatedNextPageToken(): string {
+    return Object
+      .entries(this.providerStreams)
+      .filter(([_, streams]) => {
+        return streams.nextPageToken !== undefined
+      })
+      .reduce<string[]>((concatenatedNextPageToken: string[], [providerId, pagedStreams]) => {
+          return [...concatenatedNextPageToken, `${providerId}:${pagedStreams.nextPageToken}`]
+    }, [])
+    .join(',')
   }
 }
--- services/tests/game_stream_service_test.ts
+++ services/tests/game_stream_service_test.ts
@@ -1,37 +1,96 @@
 import { assertEquals } from "../../dev_deps.ts"
-import { GameStreamService, GameStreamProvider, Stream } from "../game_stream_service.ts";
+import { GameStreamService, GameStreamProvider, PagedStreams, Stream, GameStreamProviderOptions } from "../game_stream_service.ts";
 
 Deno.test("Game stream service", async (t) => {
   await t.step("returns an empty list of game streams when the stream provider has no streams available", () => {
-    const streamProvider = new TestStreamProvider([])
+    const streamProvider = new TestStreamProvider('1', createPagedStreams(0))
     const streamService = new GameStreamService().registerStreamProvider(streamProvider)
 
-    assertEquals(streamService.getStreams().length, 0)
+    const pagedStreams = streamService.getStreams({ pageSize: 1, pageSkip: 0 })
+
+    assertEquals(pagedStreams.streams.length, 0)
   })
 
-  await t.step("returns a list of game streams when the stream provider has streams available", () => {
+  await t.step("returns a list of game streams when at least one stream provider has streams available", () => {
     const streamService = new GameStreamService()
-      .registerStreamProvider(new TestStreamProvider([{}, {}]))
+      .registerStreamProvider(new TestStreamProvider('1', createPagedStreams(2)))
 
-    assertEquals(streamService.getStreams().length, 2)
+    const pagedStreams = streamService.getStreams({ pageSize: 2, pageSkip: 0 })
+
+    assertEquals(pagedStreams.streams.length, 2)
   })
 
   await t.step("returns a list of game streams from all providers", () => {
     const streamService = new GameStreamService()
-      .registerStreamProvider(new TestStreamProvider([{}, {}]))
-      .registerStreamProvider(new TestStreamProvider([{}]))
+      .registerStreamProvider(new TestStreamProvider('1', createPagedStreams(2)))
+      .registerStreamProvider(new TestStreamProvider('2', createPagedStreams(1)))
 
-    assertEquals(streamService.getStreams().length, 3)
+    const pagedStreams = streamService.getStreams({ pageSize: 2, pageSkip: 0 })
+
+    assertEquals(pagedStreams.streams.length, 3)
   })
+
+  await t.step("returns a paged list of streams from each provider", () => {
+    const streamService = new GameStreamService()
+      .registerStreamProvider(new TestStreamProvider('1', createPagedStreams(3)))
+      .registerStreamProvider(new TestStreamProvider('2', createPagedStreams(3)))
+
+    const pagedStreams = streamService.getStreams({ pageSize: 2, pageSkip: 0 })
+
+    assertEquals(pagedStreams.streams.length, 4)
+  })
+  await t.step("returns paged list of streams from each provider starting from a page offset", () => {
+    const streamService = new GameStreamService()
+      .registerStreamProvider(new TestStreamProvider('1', createPagedStreams(3)))
+      .registerStreamProvider(new TestStreamProvider('2', createPagedStreams(3)))
+
+    const pagedStreams = streamService.getStreams({ pageSize: 2, pageSkip: 1 })
+
+    assertEquals(pagedStreams.streams.length, 2)
+  })
+  await t.step("returns a next page token for each provider that has more pages", () => {
+    const streamService = new GameStreamService()
+      .registerStreamProvider(new TestStreamProvider('1', createPagedStreams(1), 'pageToken1'))
+      .registerStreamProvider(new TestStreamProvider('2', createPagedStreams(1)))
+      .registerStreamProvider(new TestStreamProvider('3', createPagedStreams(1), 'pageToken3'))
+      
+
+    const pagedStreams = streamService.getStreams({ pageSize: 2, pageSkip: 0 })
+
+    assertEquals(pagedStreams, {
+      streams: [{}, {}, {}],
+      nextPageToken: '1:pageToken1,3:pageToken3'
+    })
+  })
+
 })
 
 class TestStreamProvider implements GameStreamProvider {
-  private streams: Stream[]
-  constructor(streams: Stream[]) {
-    this.streams = streams
+  private providerId: string
+  private pagedStreams: PagedStreams
+  private nextPageToken?: string
+
+  constructor(providerId: string, pagedStreams: PagedStreams, nextPageToken?: string) {
+    this.pagedStreams = pagedStreams
+    this.nextPageToken = nextPageToken
+    this.providerId = providerId
+  }
+  public get id(): string {
+      return this.providerId
   }
 
-  getStreams(): Stream[] {
-    return this.streams
+  getStreams(options: GameStreamProviderOptions): PagedStreams {
+    return {
+      streams: this.pagedStreams.streams.slice(options.pageSkip, options.pageSize),
+      nextPageToken: this.nextPageToken
+    }
+  }
+}
+
+function createPagedStreams(numberOfStreams: number): PagedStreams {
+  const streams = new Array<Stream>(numberOfStreams).fill({})
+
+  return {
+    streams,
   }
 }
